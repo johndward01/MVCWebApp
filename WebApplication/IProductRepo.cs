0997527618755f6393a06b054516674a140118bb
johndward01 <johndward01@gmail.com>
2022-06-01 13:28:01 -0500

    Add project files.

---
+++ WebApplication/IProductRepo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+    }
+}
